c0e7779c68beff357c90ea4e058128ee9704c685
James Mundy <james@foundbite.co>
2016-06-26 17:36:34 +0100

    Target iOS 9.0

---
+++ XcodeProject/STPopup/DerivedData/STPopup/Build/Intermediates/STPopup.build/Release-iphoneos/STPopup.build/Objects-normal/armv7/STPopupNavigationBar.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/james/Documents/GitHub/Xamarin.STPopup/XcodeProject/STPopup/STPopup/STPopupNavigationBar.m \
+  /Users/james/Documents/GitHub/Xamarin.STPopup/XcodeProject/STPopup/STPopup/STPopupNavigationBar.h
